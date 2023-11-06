ae831c1f803579505fea9b8517e984ac3d8a6e9d
Megan <mvyenielo58@gmail.com>
2023-11-06 15:07:37 -0800

    completed Stack class and methods

--- stack.js
+++ stack.js
@@ -19,26 +19,40 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
+    const newNode = new Node(val);
 
+    const top = this.top;
+    this.top = newNode;
+    newNode.next = top;
+
+    this.size+=1;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if(this.isEmpty()) throw new Error("Stack is empty");
 
+    const top = this.top
+    this.top = this.top.next;
+    this.size -= 1;
+
+    return top.val;
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
 
+    return this.top.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
 
+    return this.size === 0;
   }
 }
 
