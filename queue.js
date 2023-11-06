ebf23fab7552b1799055ca67be7ef38769a052e9
ross-brown <rbrown_98@yahoo.com>
2023-11-06 16:54:20 -0600

    finish queue class

--- queue.js
+++ queue.js
@@ -20,26 +20,41 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    const newNode = new Node(val);
+    this.size += 1;
 
+    if (!this.first) {
+      this.first = this.last = newNode;
+      return;
+    }
+
+    const last = this.last;
+    last.next = newNode;
+    this.last = newNode;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size === 0) throw new Error("Queue is empty");
+    this.size -= 1;
 
+    const first = this.first;
+    this.first = this.first.next;
+    return first.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
