8205432833d62bc3f95b16b76d2274253e5bcbe2
ross-brown <rbrown_98@yahoo.com>
2023-11-06 19:26:51 -0600

    finish string reverse and balance bracket questions

--- further-study/further-study.js
+++ further-study/further-study.js
@@ -1,10 +1,48 @@
 const Queue = require("../queue");
 const Stack = require("../stack");
 
+
+
+// "Write a function that reverses a string by handling one letter at a time."
+// "You cannot use an arrays, nor can you use any string-reversal built-in method."
+
 function stringReversal(str) {
+  const letterStack = new Stack();
+  let reversed = "";
+
+  for (const letter of str) {
+    letterStack.push(letter);
+  }
+
+  while (letterStack.size !== 0) {
+    reversed += letterStack.pop();
+  }
+
+  return reversed;
 }
 
 function balancedBrackets(str) {
+  const bracketStack = new Stack();
+
+  const brackets = {
+    "{": "}",
+    "(": ")",
+    "[": "]"
+  };
+
+  for (const char of str) {
+    if (char === "{" || char === "(" || char === "[") {
+      bracketStack.push(char);
+    } else if (char === "}" || char === ")" || char === "]") {
+      if (brackets[bracketStack.peek()] === char) {
+        bracketStack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+    return bracketStack.isEmpty();
 }
 
 function josephusSurvivor(peopleNum, skip) {
